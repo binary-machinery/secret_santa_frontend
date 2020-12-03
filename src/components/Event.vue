fe47e272df38813133a75aecc88bd97079bde875
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-12-03 23:53:12 +0200

    Personal event data

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -2,6 +2,14 @@
   <div>
     <h2>{{ event.name }}</h2>
     <p>{{ event.description }}</p>
+
+    <h3>Мои пожелания</h3>
+    <p>{{ personalData.wishes }}</p>
+
+    <h3>Получатель</h3>
+    <p>{{ personalData.receiver_name }}</p>
+    <p>{{ personalData.receiver_wishes }}</p>
+
     <h3>Участники</h3>
     <table>
       <tr v-for="eventUser in eventUsers" :key="eventUser.user_id">
@@ -22,18 +30,28 @@ export default {
   data: function () {
     return {
       event: null,
-      eventUsers: []
+      eventUsers: [],
+      personalData: null
     }
   },
   mounted: function () {
-    Axios.get(this.SERVER_URL + '/event/' + this.$route.params.eventId, { withCredentials: true })
+    Axios.get(this.SERVER_URL + '/event/' + this.$route.params.eventId,
+        { withCredentials: true })
         .then(response => {
           this.event = response.data;
         });
-    Axios.get(this.SERVER_URL + '/event/' + this.$route.params.eventId + '/users', { withCredentials: true })
+
+    Axios.get(this.SERVER_URL + '/event/' + this.$route.params.eventId + '/users',
+        { withCredentials: true })
         .then(response => {
           this.eventUsers = response.data;
-        })
+        });
+
+    Axios.get(this.SERVER_URL + '/event/' + this.$route.params.eventId + '/personal-data',
+        { withCredentials: true })
+        .then(response => {
+          this.personalData = response.data;
+        });
   }
 }
 </script>
